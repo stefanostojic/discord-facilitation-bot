9b2b0fd195d46e8660c2b17b85b3895794e101c6
Stefan Ostojic <stefanostojic98@gmail.com>
2021-01-17 16:08:15 +0100

    third fix

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
-
+ 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
